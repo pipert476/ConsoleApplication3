ebaae6bbfa1d003c1cb716f4d6a724e1d3e142a9
piper <piper@PIPERT>
2022-11-18 14:42:15 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,34 @@
+﻿
+
+#include <iostream>
+#include <locale>
+#include <conio.h>
+#include <cmath>
+
+using namespace std;
+
+
+
+int main()
+{
+	setlocale(0, ".1251");
+
+	
+	cout << "\n Пишете число у радiанах, натискаєте вiдступ, а потiм ENTER \n\n";
+
+	int x;
+
+	x = 0;
+
+	while (true) 
+	{
+
+		cin >> x;
+
+		cout << x * 180 / 3.14159265359;
+
+	}
+
+
+}
+
